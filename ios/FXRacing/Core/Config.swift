6808f5975742062bf17ca78a0fbaa8ee5fc10af8
gibouu <115729624+gibouu@users.noreply.github.com>
2026-05-20 22:27:15 -0400

    Use local API for debug iOS builds (#68)
    Closes #67

--- ios/FXRacing/Core/Config.swift
+++ ios/FXRacing/Core/Config.swift
@@ -2,11 +2,11 @@ import Foundation
 
 enum Config {
     #if DEBUG
+    static let apiBaseURL = URL(string: "http://127.0.0.1:3000")!
+    #else
     /// Use the canonical production host. Avoid the apex domain here:
     /// if it redirects to `www`, URLSession may drop the Authorization header
     /// on the redirected request and mobile Bearer auth will fail.
-    static let apiBaseURL = URL(string: "https://www.fxracing.ca")!
-    #else
     static let apiBaseURL = URL(string: "https://www.fxracing.ca")!
     #endif
 }
